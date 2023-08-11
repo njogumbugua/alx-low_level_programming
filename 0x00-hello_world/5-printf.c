1f69d5a532f01e8578b90030879638085978c836
Njogu Mbugua <jeremyirara@gmail.com>
2023-08-10 20:06:12 -0700

    add 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <sdio.h>
+/**
+ * main - A program that prints a line with pringf function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
