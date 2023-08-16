2a345d1c3f036ef0f8f2b8940f8ade4de93cfadd
Conrad Njogu <Njogu Mbugua>
2023-08-16 13:01:53 +0300

    simplified 7-print_last_digit.c file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,22 +6,7 @@
  */
 int print_last_digit(int n)
 {
-int a;
-
-if (n < 0)
-{
-n = -n;
-}
-
-a = n % 10;
-
-if (a < 0)
-{
-a = -a;
-}
-
-_putchar(a + '0');
-
-return (a);
+    int lastDigit = n % 10;
+    return (lastDigit);
 }
 
