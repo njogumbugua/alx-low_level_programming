d2a6c606916e697922a484c9c0248f0d45c8338e
Conrad Njogu <Njogu Mbugua>
2023-08-16 14:12:28 +0300

    simplify 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,34 +8,13 @@
  */
 void print_to_98(int n)
 {
-int i, j;
-
-if (n <= 98)
-{
-for (i = n; i <= 98; i++)
-{
-if (i != 98)
-{
-printf("%d, ", i);
-}
-else if (i == 98)
-{
-printf("%d\n", i);
+while (n != 98) {
+printf("%d, ", n);
+if (n < 98) {
+    n++;
+} else {
+    n--;
 }
 }
-}
-else if (n >= 98)
-{
-for (j = n; j >= 98; j--)
-{
-if (j != 98)
-{
-printf("%d, ", j);
-}
-else if (j == 98)
-{
-printf("%d\n", j);
-}
-}
-}
+printf("98\n");
 }
