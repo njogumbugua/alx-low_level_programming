d0e681ce4ee3c0879f59bb56509713ebc3a0027d
Conrad Njogu <Njogu Mbugua>
2023-09-01 05:21:15 +0300

    modify main.h file

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,7 @@ void _puts_recursion(char *s);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int _strlen_recursion(char *s);
+int _putchar(char c);
 int factorial(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
