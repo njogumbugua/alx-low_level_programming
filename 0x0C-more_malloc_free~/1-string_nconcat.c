3f5889fdf3becd3622be77a8d5b8b606eae1da8f
Conrad Njogu <Njogu Mbugua>
2023-09-08 09:09:15 +0300

    remove spaces in 1-string_nconcat.c file

--- 0x0C-more_malloc_free~/1-string_nconcat.c
+++ 0x0C-more_malloc_free~/1-string_nconcat.c
@@ -16,34 +16,34 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  if (s1 == NULL)
-  {
-    s1 = "";
-  }
+if (s1 == NULL)
+{
+s1 = "";
+}
 
-  if (s2 == NULL)
-  {
-    s2 = "";
-  }
+if (s2 == NULL)
+{
+s2 = "";
+}
 
-  size_t len1 = strlen(s1);
-  size_t len2 = strlen(s2);
+size_t len1 = strlen(s1);
+size_t len2 = strlen(s2);
 
-  if(n >= len2)
-  {
-    n = len2;
-  }
+if(n >= len2)
+{
+n = len2;
+}
 
-  char *result = malloc(len1 + n + 1);
+char *result = malloc(len1 + n + 1);
 
-  if (result == NULL)
-  {
-    return (NULL);
-  }
+if (result == NULL)
+{
+return (NULL);
+}
 
-  strcpy(result, s1);
-  strcat(result, s2);
+strcpy(result, s1);
+strcat(result, s2);
 
-  return (result);
-  free(result);
+return (result);
+free(result);
 }
