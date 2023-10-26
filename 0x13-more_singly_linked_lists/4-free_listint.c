36bd7782b3a529bcbc24038ec4c44707660c15b5
Conrad Njogu <Njogu Mbugua>
2023-10-26 06:16:52 +0300

    fix betty errors

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,11 +8,11 @@
 
 void free_listint(listint_t *head)
 {
-  listint_t *temp;
-  while (head)
-  {
-    temp = head->next;
-    free(head);
-    head = temp;
-  }
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
